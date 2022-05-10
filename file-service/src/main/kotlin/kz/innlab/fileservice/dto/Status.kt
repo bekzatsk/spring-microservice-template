c7b42716ac0d5f59fb656bd6e79e094c6b635896
bekzatsa <bekzat.saylaubay@gmail.com>
2022-05-10 15:07:56 +0600

    Docker

--- file-service/src/main/kotlin/kz/innlab/fileservice/dto/Status.kt
+++
@@ -1,7 +0,0 @@
-package kz.innlab.fileservice.dto
-
-data class Status (
-    var status: Int = 0,
-    var message: String? = "Something went wrong please try again!",
-    var value: Any? = null
-)
--- user-service/src/main/kotlin/kz/innlab/userservice/dto/Status.kt
+++
@@ -1,7 +0,0 @@
-package kz.innlab.userservice.dto
-
-data class Status (
-    var status: Int = 0,
-    var message: String? = "Something went wrong please try again!",
-    var value: Any? = null
-)
